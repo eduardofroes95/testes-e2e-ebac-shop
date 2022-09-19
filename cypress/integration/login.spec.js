44241508cbedb71a49fcf93602a50f7ce4aa1fea
Eduardo Almeida <eduardofroes95@gmail.com>
2022-09-18 21:33:41 -0300

    Updates

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -32,7 +32,6 @@ context('Funcionalidade Login', () => {
         cy.get('#username').type(dadosLogin.usuario)
         cy.get('#password').type(dadosLogin.senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.page-title').should('contain', 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        cy.get('.woocommerce-MyAccount-navigation-link--customer-logout > a').should('contain', 'Sair')
     })
 })
